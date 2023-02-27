ef5f097694e14c35992acf56c5f7af74b05f530a
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-02-27 17:33:24 -0300

    docs: Create products seeder, migration and model

---
+++ back-end/src/database/migrations/20230227195055-create-product.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      price: {
+        allowNull: false,
+        type: Sequelize.DOUBLE,
+      },
+      urlImage: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'ur_image',
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('products');
+  }
+};
