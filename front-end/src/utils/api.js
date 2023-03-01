6ca42464e02fe0c0d2a560327e5e0292edf3a1b0
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-03-01 19:23:44 -0300

    feat: Create get method

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 const api = axios.create({ URL: 'http://localhost:3001' });
 
 const post = async (route, data) => api.post(`/${route}`, data);
-const d = '';
+const get = async (route) => api.get(`/${route}`);
 
-export { post, d };
+export { post, get };
