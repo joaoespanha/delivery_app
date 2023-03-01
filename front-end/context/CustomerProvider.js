e25126cdc125355bf0aa363b5cde4a3ec54fbf15
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-03-01 17:39:07 -0300

    feat: Create Customer Context

---
+++ front-end/context/CustomerProvider.js
@@ -0,0 +1,26 @@
+import React, { useState, useMemo } from 'react';
+import PropTypes from 'prop-types';
+import customerContext from './CustomerContext';
+
+function CustomerProvider({ children }) {
+  const [shop, setShop] = useState([]);
+
+  const contextValues = {
+    shop,
+    setShop,
+  };
+
+  const memorizedContextValue = useMemo(() => contextValues, [shop]);
+
+  return (
+    <customerContext.Provider value={ memorizedContextValue }>
+      { children }
+    </customerContext.Provider>
+  );
+}
+
+CustomerProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default CustomerProvider;
