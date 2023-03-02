cb89ba21eb7f196d4573ac2552176d310fcc5ed3
Joao Espanha <jpbesr@gmail.com>
2023-03-02 17:24:39 -0300

    feat: implementing getAll products route

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const loginRouter = require('./login.routes');
 const registerRouter = require('./register.routes');
+const productsRouter = require('./products.routes');
 
 const router = express.Router();
 
 router.use('/login', loginRouter);
 router.use('/register', registerRouter);
+router.use('/products', productsRouter);
 
 module.exports = router;
---
+++ back-end/src/routes/products.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+
+const productsController = require('../controller/products.controller');
+const { validateTokenMiddle } = require('../middlewares/tokenValidation.middlewares');
+
+router.get('/',validateTokenMiddle, productsController.getAll);
+
+module.exports = router;
