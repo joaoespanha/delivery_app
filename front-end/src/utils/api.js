1eef4a2d39816c045182da67b17e51e27552f6af
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-03-02 23:24:05 -0300

    refactor: Refactor get axios method

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 const URL = 'http://localhost:3001';
 
 const post = async (route, data) => axios.post(`${URL}/${route}`, data);
-const get = async (route) => axios.get(`${URL}/${route}`);
+const get = async (route, headers) => axios.get(`${URL}/${route}`, headers);
 
 export { post, get };
