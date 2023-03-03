920a12b0478bc5098c188a71181e69058b71cfa0
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-03-03 19:27:45 -0300

    refactor: Refactor post axios method

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const URL = 'http://localhost:3001';
 
-const post = async (route, data) => axios.post(`${URL}/${route}`, data);
+const post = async (route, data, headers) => axios.post(`${URL}/${route}`, data, headers);
 const get = async (route, headers) => axios.get(`${URL}/${route}`, headers);
 
 export { post, get };
