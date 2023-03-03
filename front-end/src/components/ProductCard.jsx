b3195e5d655a60f8beffd4576c444ea2a36d40f4
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-03-03 19:30:05 -0300

    fix: change proptypes value

--- front-end/src/components/ProductCard.jsx
+++ front-end/src/components/ProductCard.jsx
@@ -129,7 +129,7 @@ ProductCard.propTypes = {
   item: PropTypes.shape({
     id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
+    price: PropTypes.string.isRequired,
     urlImage: PropTypes.string.isRequired,
   }).isRequired,
 };
