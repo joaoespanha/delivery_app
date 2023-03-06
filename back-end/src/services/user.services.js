d3c1fd536453f0ae39052749f83877458c718bbf
Joao Espanha <jpbesr@gmail.com>
2023-03-06 16:03:38 -0300

    feat: implementing getAllUsersByRole route

---
+++ back-end/src/services/user.services.js
@@ -0,0 +1,14 @@
+const { User } = require('../database/models');
+
+
+
+
+const getAllByRole = async (role) => {
+    const users = await User.findAll({ where: { role } });
+
+    return { type: null, message: users, status: 200 };
+};
+
+module.exports = {
+    getAllByRole,
+};
