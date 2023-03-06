da13b89bbdef65b4104e88921339fd7699925ad5
Cesar Nogueira <cesardinogueira@gmail.com>
2023-03-06 19:25:10 -0300

    fix: change totalprice model sales from double to decimal

--- back-end/src/database/models/Sales.model.js
+++ back-end/src/database/models/Sales.model.js
@@ -3,7 +3,7 @@ module.exports = (sequelize, DataTypes) => {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     userId: { type: DataTypes.INTEGER, foreignKey: true },
     sellerId: { type: DataTypes.INTEGER, foreignKey: true },
-    totalPrice: DataTypes.DOUBLE,
+    totalPrice: DataTypes.DECIMAL(4,2),
     deliveryAddress: DataTypes.STRING,
     deliveryNumber: DataTypes.STRING,
     saleDate: DataTypes.DATE,
