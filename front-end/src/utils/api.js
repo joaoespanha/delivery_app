0db4b8c13b7e2054609e869363ac60152103be8b
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-03-21 12:44:58 -0300

    preparando deploy

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -1,4 +1,4 @@
-/* import axios from 'axios';
+import axios from 'axios';
 
 const URL = 'https://back-end-production-9f66.up.railway.app';
 
@@ -8,37 +8,5 @@ const destroy = async (route, data, headers) => axios
 const get = async (route, headers) => axios.get(`${URL}/${route}`, headers);
 const patch = async (route, data, headers) => axios
   .patch(`${URL}/${route}`, data, headers);
-
-export { post, get, patch, destroy };
- */
-import axios from 'axios';
-
-const URL = 'https://back-end-production-9f66.up.railway.app';
-
-const post = async (route, data, headers) => axios.post(`${URL}/${route}`, data, {
-  headers: {
-    ...headers,
-    'Access-Control-Allow-Origin': '*',
-  },
-});
-const destroy = async (route, data, headers) => axios.delete(`${URL}/${route}`, {
-  headers: {
-    ...headers,
-    'Access-Control-Allow-Origin': '*',
-  },
-  data,
-});
-const get = async (route, headers) => axios.get(`${URL}/${route}`, {
-  headers: {
-    ...headers,
-    'Access-Control-Allow-Origin': '*',
-  },
-});
-const patch = async (route, data, headers) => axios.patch(`${URL}/${route}`, data, {
-  headers: {
-    ...headers,
-    'Access-Control-Allow-Origin': '*',
-  },
-});
 
 export { post, get, patch, destroy };
