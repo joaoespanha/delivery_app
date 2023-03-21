d400f84195506819436480cff42ef327c255d261
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-03-21 19:31:00 -0300

    tentativa de deploy

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -1,13 +1,12 @@
 import axios from 'axios';
 
-/* const HOST = process.env.REACT_APP_API_HOST || 'back-end-production-9f66.up.railway.app'; */
 const HOST = process.env.REACT_APP_API_HOST || 'localhost:3001';
 const PROTOCOL = process.env.REACT_APP_API_PROTOCOL || 'http';
 
-const BASE_URL = `${PROTOCOL}://${HOST}`;
+const URL = `${PROTOCOL}://${HOST}`;
 
 const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: URL,
   timeout: 10000,
   headers: {
     Accept: 'application/json',
@@ -15,19 +14,15 @@ const api = axios.create({
   },
 });
 
-/* const URL = 'https://back-end-production-9f66.up.railway.app';
- */
-console.log(BASE_URL);
-const post = async (route, data, headers) => api.post(
-  `${BASE_URL}/${route}`,
-  data,
-  headers,
-)
-|| console.log('isso é a URL', BASE_URL);
+/* const URL = 'https://back-end-production-9f66.up.railway.app'; */
+
+console.log(URL);
+const post = async (route, data, headers) => api.post(`${URL}/${route}`, data, headers)
+|| console.log('isso é a URL', URL);
 const destroy = async (route, data, headers) => api
-  .delete(`${BASE_URL}/${route}`, data, headers);
-const get = async (route, headers) => api.get(`${BASE_URL}/${route}`, headers);
+  .delete(`${URL}/${route}`, data, headers);
+const get = async (route, headers) => api.get(`${URL}/${route}`, headers);
 const patch = async (route, data, headers) => api
-  .patch(`${BASE_URL}/${route}`, data, headers);
+  .patch(`${URL}/${route}`, data, headers);
 
 export { post, get, patch, destroy };
