f6fa148852ee1fd1869f730b681a7cf027d62ff4
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-03-22 15:03:41 -0300

    tentativa de alteração cors com auxilio instrutor Ivan Rafael da Trybe

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,15 +1,17 @@
-const express = require('express');
 const cors = require('cors');
+const express = require('express');
 const path = require('path');
 const routes = require('../routes');
 
 const app = express();
 
-const imagePath = path.join(__dirname, '..', '..', '..', 'assets', 'public');
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
-app.use(cors({
-  origin: '*'
-}));
+const imagePath = path.join(__dirname, '..', '..', '..', 'assets', 'public');
 
 app.use(express.json());
 
